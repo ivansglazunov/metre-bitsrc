fcc0cded9e245e455f6d4bf945be45a103b3c0a7
Ivan S Glazunov <ivansglazunov@gmail.com>
2019-03-21 14:24:45 +0300

    fix to meteor names

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 import "normalize.css";
 
 import { Firstname } from "./firstname.jsx";
+import { UserPosts } from "./userposts.jsx";
 
 const random = () =>
   Math.random()
@@ -17,11 +18,12 @@ class App extends React.Component {
     user: null
   };
   toggle = () => this.setState(({ open }) => ({ open: !open }));
-  createAndLogin = () =>
-    this.setState(() => ({ user: { id: "a", firstname: random() } }));
+  createAndLogin = () => this.setState(() => ({ user: { _id: "a", profile: { firstname: random() }, posts: [] } }));
   logout = () => this.setState(() => ({ user: null }));
-  random = () =>
-    this.setState(() => ({ user: { id: "a", firstname: random() } }));
+  random = () => this.setState(({ user }) => ({ user: { ...user, profile: { firstname: random() } } }));
+  createRandomPost = () => this.setState(({ user }) => ({ user: { ...user, posts: [...user.posts, { _id: random(), content: random() }] } }));
+  removePost = (id) => this.setState(({ user }) => ({ user: { ...user, posts: user.posts.filter(post => post._id !== id) } }));
+  
   render() {
     const { open, user } = this.state;
 
@@ -36,10 +38,17 @@ class App extends React.Component {
           logout={this.logout}
           random={this.random}
         />
+        <UserPosts
+          name={user && user.profile && user.profile.firstname}
+          posts={user ? user.posts : []}
+          createRandom={this.createRandomPost}
+          remove={this.removePost}
+        />
       </div>
     );
   }
 }
 
+console.log(document);
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
--- src/userposts.jsx
+++ src/userposts.jsx
@@ -9,9 +9,9 @@ export const UserPosts = ({
   return <div>
     <button onClick={createRandom}>createRandom</button> for {name}
     <ul>
-      {posts && posts.map(post => <li key={post.id}>
+      {posts && posts.map(post => <li key={post._id}>
         {post.content}
-        <button onClick={() => remove(post.id)}>remove</button>
+        <button onClick={() => remove(post._id)}>remove</button>
       </li>)}
     </ul>
   </div>;
