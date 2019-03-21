661447c68117e6dba92203ffcebb53fccab51675
Ivan S Glazunov <ivansglazunov@gmail.com>
2019-03-21 14:31:08 +0300

    proptypes

--- src/firstname.jsx
+++ src/firstname.jsx
@@ -1,33 +1,48 @@
 import * as React from "react";
+import PropTypes from 'prop-types';
 
-export const Firstname = ({
-  open,
-  toggle,
-  user,
-  create,
-  login,
-  logout,
-  random
-}) => {
-  return (
-    <div>
-      <button onClick={toggle}>{open ? "hide" : "show"}</button>
-      {open && (
-        <React.Fragment>
-          {user ? (
-            <React.Fragment>
-              <button onClick={logout}>logout</button>
-              <button onClick={random}>random</button>
-            </React.Fragment>
-          ) : (
-            <React.Fragment>
-              <button onClick={create}>create</button>
-              <button onClick={login}>login</button>
-            </React.Fragment>
-          )}
-          <div>{JSON.stringify(user)}</div>
-        </React.Fragment>
-      )}
-    </div>
-  );
+export class Firstname extends React.Component {
+  static propTypes = {
+    user: PropTypes.object.isRequired,
+    open: PropTypes.bool.isRequired,
+    toggle: PropTypes.func.isRequired,
+    create: PropTypes.func.isRequired,
+    login: PropTypes.func.isRequired,
+    logout: PropTypes.func.isRequired,
+    random: PropTypes.func.isRequired,
+  };
+
+  render() {
+    const {
+      user,
+      open,
+      toggle,
+      create,
+      login,
+      logout,
+      random,
+    } = this.props;
+    
+    return (
+      <div>
+        <button onClick={toggle}>{open ? "hide" : "show"}</button>
+        {open && (
+          <React.Fragment>
+            {user ? (
+              <React.Fragment>
+                <button onClick={logout}>logout</button>
+                <button onClick={random}>random</button>
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
+                <button onClick={create}>create</button>
+                <button onClick={login}>login</button>
+              </React.Fragment>
+            )}
+            <div>{JSON.stringify(user)}</div>
+          </React.Fragment>
+        )}
+      </div>
+    );
+  }
 };
--- src/userposts.jsx
+++ src/userposts.jsx
@@ -1,18 +1,35 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 
-export const UserPosts = ({
-  name,
-  posts,
-  remove,
-  createRandom,
-}) => {
-  return <div>
-    <button onClick={createRandom}>createRandom</button> for {name}
-    <ul>
-      {posts && posts.map(post => <li key={post._id}>
-        {post.content}
-        <button onClick={() => remove(post._id)}>remove</button>
-      </li>)}
-    </ul>
-  </div>;
+export class UserPosts extends React.Component {
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    posts: PropTypes.oneOf(
+      PropTypes.shape({
+        _id: PropTypes.string.isRequired,
+        content: PropTypes.string.isRequired,
+      }),
+    ).isRequired,
+    remove: PropTypes.func.isRequired,
+    createRandom: PropTypes.func.isRequired,
+  };
+
+  render() {
+    const {
+      name,
+      posts,
+      remove,
+      createRandom,
+    } = this.props;
+    
+    return <div>
+      <button onClick={createRandom}>createRandom</button> for {name}
+      <ul>
+        {posts && posts.map(post => <li key={post._id}>
+          {post.content}
+          <button onClick={() => remove(post._id)}>remove</button>
+        </li>)}
+      </ul>
+    </div>;
+  }
 };
